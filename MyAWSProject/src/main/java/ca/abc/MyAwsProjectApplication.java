e1bdf462c9b5043ea4ed40d3ba4587561e2879d1
Code <code@codeMonkE.com>
2021-02-14 19:47:04 -0500

    first commit

---
+++ MyAWSProject/src/main/java/ca/abc/MyAwsProjectApplication.java
@@ -0,0 +1,13 @@
+package ca.abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAwsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAwsProjectApplication.class, args);
+	}
+
+}
---
+++ MyAWSProject/src/test/java/ca/abc/MyAwsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.abc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAwsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
